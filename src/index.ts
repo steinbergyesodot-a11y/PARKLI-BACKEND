2a37b5e841d47b7ec16200e9e1060460d7809774
Yosef Steinberg <steinbergyosef@gmail.com>
2026-03-10 20:17:00 +0200

    added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const connect = async () => {
+    console.log("DATABASE_URI from Render:", JSON.stringify(process.env.DATABASE_URI));
+
     const dbURL = process.env.DATABASE_URI
     if(!dbURL){
         throw new Error('error')
