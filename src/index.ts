83bec5678f93078e5143eab8590278b93be1bebe
Yosef Steinberg <steinbergyosef@gmail.com>
2026-03-12 14:52:35 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,19 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const connect = async () => {
-    console.log("DATABASE_URI from Render:", JSON.stringify(process.env.DATABASE_URI));
 
     const dbURL = process.env.DATABASE_URI
     if(!dbURL){
         throw new Error('error')
     }
     mongoose.connection.on("error", err => {
-    console.error("❌ Mongoose runtime error:", err);
 });
 
     try{
         console.log("ENV DATABASE_URI:", process.env.DATABASE_URI);
 
         await mongoose.connect(dbURL)
-        
-
-        console.log("Connected to MongoDB:", process.env.DATABASE_URI);
+        console.log("Connected DB:", mongoose.connection.name)
 
     }catch(error){
         console.error("❌ MongoDB connection error:");
