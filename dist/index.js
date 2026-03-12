68d97ca89391844f42524a420d721969fdace542
Yosef Steinberg <steinbergyosef@gmail.com>
2026-03-12 14:55:10 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -7,18 +7,16 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const connect = async () => {
-    console.log("DATABASE_URI from Render:", JSON.stringify(process.env.DATABASE_URI));
     const dbURL = process.env.DATABASE_URI;
     if (!dbURL) {
         throw new Error('error');
     }
     mongoose_1.default.connection.on("error", err => {
-        console.error("❌ Mongoose runtime error:", err);
     });
     try {
         console.log("ENV DATABASE_URI:", process.env.DATABASE_URI);
         await mongoose_1.default.connect(dbURL);
-        console.log("Connected to MongoDB:", process.env.DATABASE_URI);
+        console.log("Connected DB:", mongoose_1.default.connection.name);
     }
     catch (error) {
         console.error("❌ MongoDB connection error:");
